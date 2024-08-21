ac8b1242e0b5f91be65a1f67c6d8f731bee590a8
raju0005 <anderajesh15@gmail.com>
2024-08-21 13:19:24 +0530

    errors

--- client/src/components/Score.jsx
+++ client/src/components/Score.jsx
@@ -2,11 +2,14 @@ import React, { useEffect } from "react";
 import { useAddScoreMutation } from "../redux/apis/userApiSlice";
 import { useSelector } from "react-redux";
 
-const Score = ({ stepsDone, stepGoal, activeMinutes }) => {
+const Score = ({ stepsDone = 0, stepGoal = 1, activeMinutes = 0 }) => {
     const { userInfo } = useSelector((state) => state.auth);
     const [addScore] = useAddScoreMutation();
 
     const calculatePoints = (stepsDone, stepGoal, activeMinutes) => {
+        if (isNaN(stepsDone) || isNaN(stepGoal) || isNaN(activeMinutes) || stepGoal === 0) {
+            return 0; // Handle invalid input values
+        }
         const stepPoints = Math.min((stepsDone / stepGoal) * 100, 100);
         const activeMinutesPoints = activeMinutes * 2;
         return stepPoints + activeMinutesPoints;
@@ -15,9 +18,8 @@ const Score = ({ stepsDone, stepGoal, activeMinutes }) => {
     const totalPoints = calculatePoints(stepsDone, stepGoal, activeMinutes);
 
     useEffect(() => {
-    
         const updateScore = async () => {
-            if (totalPoints) {
+            if (!isNaN(totalPoints) && totalPoints > 0 && userInfo?._id) {
                 try {
                     await addScore({ userId: userInfo._id, score: totalPoints }).unwrap();
                 } catch (error) {
@@ -27,12 +29,12 @@ const Score = ({ stepsDone, stepGoal, activeMinutes }) => {
         };
 
         updateScore();
-    }, [totalPoints, userInfo._id, addScore]);
+    }, [totalPoints, userInfo?._id, addScore]);
 
     return (
-        <div className=" min-w-[70%] flex flex-col justify-center items-center gap-3 mt-6 text-white px-10 py-3 border border-white rounded-lg bg-black/20 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 text-center">
+        <div className="min-w-[70%] flex flex-col justify-center items-center gap-3 mt-6 text-white px-10 py-3 border border-white rounded-lg bg-black/20 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 text-center">
             <h2 className="text-[25px] text-center">Total Points</h2>
-            <h3 className="text-[30px] font-custom">{totalPoints}</h3>
+            <h3 className="text-[30px] font-custom">{isNaN(totalPoints) ? '0' : totalPoints}</h3>
         </div>
     );
 };

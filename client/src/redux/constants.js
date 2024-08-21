8f0c8c7bcdb6a4609d8bc49f5b00f344b89cfb3b
raju0005 <anderajesh15@gmail.com>
2024-08-21 13:15:47 +0530

    errors

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const STEPS_URL = "/api/steps";
-export const ACTIVITIES_URL = "/api/activities";
-export const LB_URL = "/api/leaderboard";
+export const USERS_URL = "/api/fitass/users";
+export const STEPS_URL = "/api/fitass/steps";
+export const ACTIVITIES_URL = "/api/fitass/activities";
+export const LB_URL = "/api/fitass/leaderboard";
